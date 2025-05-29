cafb76de5e98d42dcbb495c6875a0410996d90d4
Бондаренко Олег <mcaleg2006@Noutbuk-Bondarenko.local>
2025-05-29 20:52:31 +0300

    goroutine

--- main.go
+++ main.go
@@ -3,23 +3,64 @@ package main
 import (
 	"fmt"
 	"sync"
-	"time"
 )
 
 func main() {
-	files := []string{"file1", "file2", "file3"}
-	fmt.Println(files)
-	var wg sync.WaitGroup
-	wg.Add(len(files))
-	for _, file := range files {
-		go func(file string) {
-			defer wg.Done()
-			fmt.Println("Починаємо завантаження...", file)
-			time.Sleep(1 * time.Second)
-			fmt.Println("Завершенно...", file)
-		}(file)
 
+	ch := make(chan string, 3)
+	var wg sync.WaitGroup
+	wg.Add(3)
+	go func() {
+		defer wg.Done()
+		ch <- "McOleg"
+	}()
+
+	go func() {
+		defer wg.Done()
+		ch <- "Zina"
+	}()
+
+	go func() {
+		defer wg.Done()
+		ch <- "love"
+	}()
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
+	for name := range ch {
+		fmt.Println(name)
 	}
-	wg.Wait()
+
+	//ch1 := make(chan string, 3)
+	//
+	//var wg sync.WaitGroup
+	//wg.Add(1)
+	//go func() {
+	//	defer wg.Done()
+	//
+	//	ch1 <- "oleg"
+	//	//fmt.Println(fmt.Sprintf("Привіт %s", i))
+	//}()
+	//wg.Add(1)
+	//go func() {
+	//	defer wg.Done()
+	//
+	//	ch1 <- "zina"
+	//
+	//}()
+	//
+	//wg.Add(1)
+	//go func() {
+	//	defer wg.Done()
+	//
+	//	ch1 <- "Koncha"
+	//}()
+	//fmt.Println(fmt.Sprintf("Hello %s", <-ch1))
+	//fmt.Println(fmt.Sprintf("Hello %s", <-ch1))
+	//fmt.Println(fmt.Sprintf("Hello %s", <-ch1))
+	//
+	//wg.Wait()
 
 }
